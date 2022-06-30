b1bfcf81e347511f264181d1cf79c23b1060e39b
smandla <35283559+smandla@users.noreply.github.com>
2022-06-30 11:38:49 -0700

    Fixed form and added script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,28 @@
+var currentTime = $("#current-time");
+var currentDate = $("#current-date");
 
-var currentTime = $('#current-time');
-var currentDate = $('#current-date');
+currentTime.text(moment().format("MMM Do, YYYY"));
+currentDate.text(moment().format("h:mm A"));
 
-currentTime.text(moment().format('MMM Do, YYYY'));
-currentDate.text(moment().format('h:mm A'));
+/**
+ * Get Project Form Information
+ */
 
+var projectNameEl = $("#project_name");
+var projectTypeEl = $("#project_type");
+var hourlyRateEl = $("#hourly_rate");
+var dueDateEl = $("#due_date");
+
+var projectData = {
+  projectName: "",
+  projectType: "",
+  hourlyRate: "",
+  dueDate: "",
+};
+projectData.projectName = projectNameEl.val();
+console.log(projectData);
+
+const init = () => {
+  //get local storage information
+};
+init();
